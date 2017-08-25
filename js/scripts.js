e162d1d93e90727329b3144a5323e92796aff51c
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-08-25 18:12:58 +0200

    Add function call / getJoke() /, add css file

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
 var url = 'http://api.icndb.com/jokes/random',
     button = document.getElementById('get-joke'),
-    paragraph= document.getElementById('joke');
+    paragraph = document.getElementById('joke');
 
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function() {
     getJoke();
 });
 
 function getJoke() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url);
-    xhr.addEventListener('load', function(){
+    xhr.addEventListener('load', function() {
         var response = JSON.parse(xhr.response);
         paragraph.innerHTML = response.value.joke;
     })
     xhr.send();
 }
+getJoke();
