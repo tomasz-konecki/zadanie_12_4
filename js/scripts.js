bc19f86f143cfa82b077cf33578cfb26cf68e905
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-08-27 12:58:29 +0200

    Update JS file

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@ var url = 'http://api.icndb.com/jokes/random',
     paragraph = document.getElementById('joke');
 
 
-button.addEventListener('click', function() {
+button.addEventListener('click', () => {
     getJoke();
 });
 
 function getJoke() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url);
-    xhr.addEventListener('load', function() {
+    xhr.addEventListener('load', () => {
         var response = JSON.parse(xhr.response);
-        paragraph.innerHTML = response.value.joke;
+        paragraph.innerText = response.value.joke;
     })
     xhr.send();
 }
